3a20019fabefc7263930195115610dc1e3ec02a9
Azhel <119928552+Azai4@users.noreply.github.com>
2024-06-16 17:19:55 +0100

    Add files via upload

---
+++ CSC 104 assignment/script.js
@@ -0,0 +1,32 @@
+let user = "AZHEL"
+alert( `welcome to CSC 104 ${user}`)
+
+console.log(user)
+
+
+let ask = prompt("what day of the week is today ? ", "Tuesday" )
+alert(ask)
+console.log(ask)
+
+//create a cgpa, compound interest, simple interest, temprature converter, currency converter'
+
+//let a = prompt();
+//let b = prompt();
+//let c = prompt();
+
+//let e = a+b+c;
+//let f = e / 100;
+let a = prompt(``);
+alert(`your answer is ${a}`)
+//let u = prompt(`your simple intrest is ${f}`)
+//alert(f)
+
+
+/*let y = prompt("input your principal",(3) )
+let z = prompt("enter your rate",(3) )
+let x = prompt()
+let v = y+z+x;
+let q = v / 100;
+let s = prompt("your simple intrest is", v)*/
+
+
